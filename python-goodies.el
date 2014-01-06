1b51b70cea49fb0181c7d79f364d811f121a6f82
thorrr <jbellthor@gmail.com>
2014-01-06 09:11:19 -0500

    undo test change

--- python-goodies.el
+++ python-goodies.el
@@ -130,7 +130,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-hook 'python-mode-hook (lambda ()
-  (setq foobar 't) 
   (set-variable 'python-indent-offset 4)
   (set-variable 'indent-tabs-mode nil)
   (setq ropemacs-enable-autoimport t)
