50c42e6cb69857c2383b815a5b7b774d1461af2e
thorrr <jbellthor@gmail.com>
2015-08-03 16:22:19 -0400

    add support for multiple python syntax checkers

--- python-goodies.el
+++ python-goodies.el
@@ -107,20 +107,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-pyflakes-init))
 (defun flymake-pyflakes-init ()
-  (if (executable-find "pyflakes")
-      (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-                         'flymake-create-temp-inplace)) 
-             (local-file (file-relative-name 
-                          temp-file 
+  (if (and (executable-find "pyflakes")
+           (executable-find "pep8"))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
                           (file-name-directory buffer-file-name))))
-        (list "pyflakes" (list local-file)))
+        (list "cmd" (list "/c"
+                          "pyflakes" local-file
+                          "&"
+                          "pep8" "--ignore=E124,E265" "--max-line-length=100" local-file
+                          )))
     (progn (message "Warning:  pyflakes executable not found") nil)
     ))
 
 (add-hook 'python-mode-hook (lambda ()
   ;;modify pyflakes' output
   ;; use \\| to separate multiple match criteria
-  (setq flymake-warn-line-regexp "imported but unused\\|unable to detect undefined names")
+  (setq flymake-warn-line-regexp "imported but unused\\|unable to detect undefined names\\|E[0-9]*")
   (setq flymake-info-line-regexp "is assigned to but never used")))
 
 (add-hook 'python-mode-hook 'turn-on-eldoc-mode)
