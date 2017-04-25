fb7f7fbe8de526ffa1ff1f953d1295e46c48f646
Jason Bell <jbellthor@gmail.com>
2017-04-24 22:12:39 -0400

    change to unix line endings

--- autocomplete.el
+++ autocomplete.el
@@ -7,6 +7,7 @@
 (require 'ac-python-async) ;; a source for python auto-complete that comes from the
                            ;; *Python* buffer or the unnamed "internal" process
 
+
 (add-hook 'python-mode-hook (lambda ()
   ;; by default emacs uses the dictionary and other buffers as
   ;; completion sources which is super lame in practice.  Turn this off.
@@ -20,9 +21,3 @@
   (yas-minor-mode)
   (add-to-list 'ac-sources 'ac-source-yasnippet)
 ))
-
-(defun check-for-readline (process)
-  "return 't if we can import readline.  if we can't autocomplete will be silently broken"
-  (if (not process) (progn (message "warning:  no process in check-for-readline") nil)
-    (let ((repl-out (python-shell-send-string-no-output "import readline;''" process)))
-      (if (string-match "^\\(>>> \\)*''" repl-out) 't nil))))
